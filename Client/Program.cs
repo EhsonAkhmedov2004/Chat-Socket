85c8d04585e35c1ea3b4761363af27d4d1a40831
Ehson <akhmedovehson2004@gmail.com>
2023-01-08 23:33:30 +0500

    Updated

---
+++ Client/Program.cs
@@ -0,0 +1,377 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+
+
+
+ new AsyncClient(); // Start client
+
+ internal struct StateObject
+ {
+    public StateObject() { }  
+    public const int BufferSize = 1024;
+    public byte[]    buffer     = new byte[BufferSize];
+
+ }
+
+public class AsyncClient
+{
+
+    public Socket       connected_client = null;
+    public const int    port = 8000;
+    public const string ip = "127.0.0.1";
+
+    public ManualResetEvent connectDone = new ManualResetEvent(false);
+
+    public AsyncClient()
+    {
+        StartClient();
+    }
+
+    void ReceiveCallback(IAsyncResult ar)
+    {
+        try
+        {
+            StateObject state = (StateObject)ar.AsyncState;
+
+            
+
+            int bytesRead = connected_client.EndReceive(ar);
+
+
+            if (bytesRead > 0)
+            {
+
+                
+                Console.WriteLine(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                connected_client.BeginReceive(state.buffer,
+                                        0,
+                                        StateObject.BufferSize,
+                                        0,
+                                        new AsyncCallback(ReceiveCallback), state);
+            }
+                
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+    void Receive()
+    {
+        try
+        {
+  
+            StateObject state = new StateObject();
+  
+ 
+            connected_client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReceiveCallback), state);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    void SendCallback(IAsyncResult ar) 
+    {
+        try
+        {     
+            int bytesSent = connected_client.EndSend(ar);
+
+            Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+    }
+
+    void Send(string data)
+    {
+
+  
+        byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+        connected_client.BeginSend(
+            byteData,
+            0,
+            byteData.Length,
+            0,
+            new AsyncCallback(SendCallback), null); 
+
+    }
+
+ 
+
+    void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+           
+
+            Socket client = (Socket)ar.AsyncState;
+
+
+            client!.EndConnect(ar);
+
+            Console.WriteLine("Connected");
+
+            connectDone.Set();
+
+
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    } 
+    
+  
+    void StartClient()
+    {
+
+        try
+        {
+            IPEndPoint IPendpoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+
+
+            Socket client = new Socket(AddressFamily.InterNetwork,
+                                       SocketType   .Stream,
+                                       ProtocolType .Tcp);
+
+
+            connected_client = client;
+
+
+            connected_client.BeginConnect(IPendpoint,
+                new AsyncCallback(ConnectCallback),client);
+           
+         
+
+            connectDone.WaitOne();
+            
+
+            
+
+            Console.WriteLine("Name");
+            var Name = Console.ReadLine();
+            while (true)
+            {
+                Receive();
+                var message = $"{Name}: {Console.ReadLine()}";
+                Send(message);
+                Receive();
+
+
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*while (true)
+{
+TcpClient tcpClient = new TcpClient("127.0.0.1", 8000);
+    NetworkStream stream = tcpClient.GetStream();
+
+    string message = Console.ReadLine();
+
+    int byteCount = Encoding.ASCII.GetByteCount(message + 1);
+    byte[] sendData = new byte[byteCount];
+    sendData = Encoding.ASCII.GetBytes(message);
+
+    
+    stream.Write(sendData, 0, sendData.Length);
+    Console.WriteLine("Sending data");
+
+  StreamReader streamReader = new StreamReader(stream);
+    string response = streamReader.ReadLine();
+
+    Console.WriteLine(response);
+    stream.Close();
+ 
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const string ip = "127.0.0.1";
+const int port = 8000;
+
+
+
+
+
+
+while (true)
+{
+
+    var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+    var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+    tcpSocket.Connect(tcpEndPoint);
+
+
+
+
+    Console.Write("Type Message > ");
+    var message = Console.ReadLine();
+
+    var data = Encoding.Unicode.GetBytes(message);
+
+    tcpSocket.Send(data);
+
+    var buff = new byte[256];
+    var size = 0;
+    var builder = new StringBuilder();
+
+
+
+    do
+    {
+        size = tcpSocket.Receive(buff);
+
+        builder.Append(Encoding.Unicode.GetString(buff, 0, size));
+
+    } while (tcpSocket.Available > 0);
+
+
+    Console.WriteLine(builder.ToString());
+    tcpSocket.Shutdown(SocketShutdown.Both);
+    tcpSocket.Close();
+
+}*/
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,384 @@
+﻿// See https://aka.ms/new-console-template for more informat
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System;
+using ConsoleApp2.Help;
+using System.Linq;
+using static System.Console;
+
+
+
+//Let's start !.....
+
+
+
+
+        // if (bytesRead > 0)
+        // {
+        //     var message = help.Frombyte(state.buffer, 0, bytesRead);
+
+        //     WriteLine(message);
+
+        //     // logic for broadcast ....
+      
+        //     foreach(var each in connections) 
+        //     {
+        //         if (each != state.listener) 
+        //         {
+        //             each.Send(help.Fromstring(message));
+        //         }
+
+        //     }
+
+
+        //     state.listener.BeginReceive(state.buffer,
+        //                          0,
+        //                          StateObject.bufferSize,
+        //                          0,
+        //                          new AsyncCallback(ReadCallback), state);
+        // }
+
+
+
+AsyncSocketListener socket = new AsyncSocketListener();
+
+public struct StateObject
+{
+    public StateObject() { }
+
+    public const int bufferSize = 1024;
+
+    public byte[] buffer = new byte[bufferSize];
+
+    // Received data string.
+    public StringBuilder sb = new StringBuilder();
+
+    public Socket listener = null;
+
+}
+class AsyncSocketListener
+{
+    public ICollection<Socket> connections = new List<Socket> { };
+    public Help help                       = new Help();
+    
+  
+
+    internal AsyncSocketListener() {
+
+        StartListening();
+    }
+
+    private static ManualResetEvent allDone = new ManualResetEvent(false);
+
+   
+
+    private void ReadCallback(IAsyncResult ar) 
+    {
+
+        StateObject state         = (StateObject)ar.AsyncState;
+
+        int         bytesRead     = state.listener.EndReceive(ar); // Got message from client....
+
+
+        if (bytesRead <= 0)
+            return;
+
+
+        var message = help.Frombyte(state.buffer, 0, bytesRead);
+
+        WriteLine(message);
+
+        // logic for broadcast ....
+      
+        foreach(var each in connections) 
+        {
+            if (each != state.listener) 
+            {
+                each.Send(help.Fromstring(message));
+            }
+
+        }
+
+
+        state.listener.BeginReceive(state.buffer,
+            0,
+            StateObject.bufferSize,
+            0,
+            new AsyncCallback(ReadCallback), state);    
+            
+
+
+    }
+    private void AcceptCallback(IAsyncResult ar)
+    {
+        allDone.Set();
+
+        Socket listener = (Socket)ar.AsyncState, handler = listener!.EndAccept(ar);
+        
+
+        StateObject state = new StateObject();
+
+        state.listener = handler;
+
+
+        connections.Add(state.listener);   
+        
+        handler.BeginReceive(
+
+            state.buffer,
+            0,
+            StateObject.bufferSize,
+            0,
+            new AsyncCallback(ReadCallback),
+            state
+
+            );
+        
+
+    }
+    private void StartListening()
+    {
+        const string ip = "127.0.0.1";
+        const int port = 8000;
+
+        IPEndPoint IPendpoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+        Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+
+        try
+        {
+            listener.Bind(IPendpoint);
+            listener.Listen(100);
+
+            while (true)
+            {
+                allDone.Reset();
+
+                WriteLine("Waiting for connection .....");
+
+                listener.BeginAccept(
+                         new AsyncCallback(AcceptCallback),
+                         listener);
+
+                allDone.WaitOne();
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const string ip = "127.0.0.1";
+
+TcpListener listener = new TcpListener(IPAddress.Parse(ip), 8000);
+listener.Start();
+Console.WriteLine("UP");
+
+while (true)
+{
+
+    Console.WriteLine("Waiting for connection!...");
+    TcpClient client = listener.AcceptTcpClient();
+    Console.WriteLine("Client accepted.");
+
+    NetworkStream stream = client.GetStream();
+    StreamReader reader = new StreamReader(client.GetStream());
+    StreamWriter writer = new StreamWriter(client.GetStream());
+
+    byte[] buffer = new byte[1024];
+    stream.Read(buffer, 0, buffer.Length);
+    int recv = 0;
+
+    foreach (byte b in buffer) {
+        if (b != 0)
+        {
+            recv++;
+        }
+    }
+
+    string req = Encoding.UTF8.GetString(buffer, 0, recv);
+    Console.WriteLine(req);
+    writer.WriteLine(req);
+    writer.Flush();
+    
+    
+
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Text;
+
+const string ip = "127.0.0.1";
+const int port = 8000;
+
+
+var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+
+tcpSocket.Bind(tcpEndPoint);
+Console.WriteLine("CHECK POINT BEFORE LISTEN METHOD !....");
+tcpSocket.Listen(10);
+Console.WriteLine("CHECK POINT AFTER LISTEN METHOD !....");
+
+
+
+while (true)
+{
+
+    var buff = new byte[256];
+    var size = 0;
+    var builder = new StringBuilder();
+
+    var listener = tcpSocket.Accept();
+    
+    Console.WriteLine("Message from client sent.....");
+
+
+    do
+    {
+
+
+        size = listener.Receive(buff);
+        builder.Append(Encoding.Unicode.GetString(buff, 0, size)); // got data -- message
+
+
+
+    } while (listener.Available > 0);
+
+    Console.WriteLine(builder.ToString());
+    listener.Send(Encoding.Unicode.GetBytes(builder.ToString()));
+    listener.Shutdown(SocketShutdown.Both);
+    listener.Close();
+
+}*/
+
+
